bd86c7eaed4437cbb75d1bb2247a2b972ad7f0a4
pavlinevtimov1998 <pavlinevtimov@yahoo.com>
2022-03-27 22:08:39 +0300

    Objects and Composition

---
+++ 3. Objects and Composition/1. Objects and Composition - Lab/06. From JSON to HTML Table.js
@@ -0,0 +1,42 @@
+function JsonToHtmlTable(json) {
+  let students = JSON.parse(json);
+  let htmlText = ["<table>"];
+  let objNames = students[0];
+
+  htmlText.push(makeKeyRow(objNames));
+  students.forEach((obj) => htmlText.push(makeValueRow(obj)));
+  htmlText.push("</table>");
+
+  function makeKeyRow(arr) {
+    let keys = [];
+    Object.keys(arr).forEach((key) => {
+      keys.push(`<th>${escapeHTML(key)}</th>`);
+    });
+    return "<tr>" + keys.join("") + "</tr>";
+  }
+
+  function makeValueRow(obj) {
+    let rows = [];
+    Object.values(obj).forEach((value) => {
+      rows.push(`<td>${escapeHTML(value)}</td>`);
+    });
+    return "<tr>" + rows.join("") + "</tr>";
+  }
+
+  function escapeHTML(value) {
+    return value
+      .toString()
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#39;");
+  }
+
+  return htmlText.join("\r\n");
+}
+
+JsonToHtmlTable(`[{"Name":"Stamat",
+"Score":5.5},
+{"Name":"Rumen",
+"Score":6}]`);
