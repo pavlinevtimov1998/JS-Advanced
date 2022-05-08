da8a4c4b39abcb1a127cb6c5ebedb8da18ff1273
pavlinevtimov1998 <pavlinevtimov@yahoo.com>
2022-05-08 23:29:53 +0300

    DOM Introduction - Lab

---
+++ 4. DOM Introduction/1. DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+  let link = document.getElementById("more");
+
+  let text = document.getElementById("text");
+
+  link.style.display = "none";
+  text.style.display = "inline";
+}
