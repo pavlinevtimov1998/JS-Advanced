636e620dd3808581ca047f8c75ccf64c0d0ab9ff
pavlinevtimov1998 <pavlinevtimov@yahoo.com>
2022-05-12 14:34:25 +0300

    DOM Manipulation and Events

---
+++ 5. DOM Manipulation and Events/1. DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -0,0 +1,7 @@
+function addItem() {
+  const input = document.getElementById("newItemText");
+  const liElement = document.createElement("li");
+  liElement.textContent = input.value;
+
+  document.getElementById("items").appendChild(liElement);
+}
